06f914ce739a78a02368d26ca5d0da25a4db9cda
Mateus Maas <maas.mateus@gmail.com>
2021-06-15 23:16:30 -0300

    Adição dos arquivos necessários para o banco de dados
    Criado o arquivo classes.py, responsável por criar as tabelas para o banco de dados, que são: Usuario e Emprestimo.
    Criado o arquivo teste.bd um banco de dados de teste para a aplicação

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, flash, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import forms
 
-# Inicialização do programa
+# Inicialização da aplicação
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emprestimos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
 app.config['SECRET_KEY'] = '35d9a306792f2de5075a4f32bfe09da6'
-
 db = SQLAlchemy(app)
 
+# Definição das rotas, métodos usados nelas e as páginas que devem ser retornadas 
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -18,6 +18,7 @@ def cadastro():
     form = forms.CadastrarUsuarioForm()
     if form.validate_on_submit():
         flash('Conta criada com sucesso!', 'success')
+        # ^ Cria um alerta para o usuário
         return redirect(url_for('index'))
     return render_template('cadastro.html', title='Cadastrar', form=form)
 
---
+++ classes.py
@@ -0,0 +1,30 @@
+from enum import unique
+from sqlalchemy.orm import backref
+from app import db
+
+# Criação das tabelas do banco de dados local
+
+class Usuario(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    cpf = db.Column(db.String(11), nullable = False, unique=True)
+    senha = db.Column(db.String(60), nullable=False)
+    salario = db.Column(db.Integer)
+    emprestimos = db.relationship('Emprestimo', backref='', lazy=True)
+
+    def __repr__(self):
+        return f"Usuário: ('{self.nome}', '{self.cpf}')"
+
+class Emprestimo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    valor = db.Column(db.Integer, nullable=False)
+    parcelas = db.Column(db.Integer, nullable=False)
+    parcelas_restantes = db.Column(db.Integer, nullable=False)
+    valor_parcela = db.Column(db.Integer, nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+
+    def __repr__(self):
+        return f"""Valor: R$ {self.valor}
+Parcelas restantes: {self.parcelas_restantes}
+Valor à ser pago: {self.valor_parcela * self.parcelas_restantes}"""
