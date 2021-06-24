061b956dc5d7c0172b8d44af636e26b760585107
Mateus Maas <maas.mateus@gmail.com>
2021-06-23 21:05:04 -0300

    Commit final
    Atualização do arquivo requirements e reinicialização do banco de dados

--- FlaskEmprestimo/__init__.py
+++ FlaskEmprestimo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 # Inicialização da aplicação
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FlaskEmprestimo.db'
 app.config['SECRET_KEY'] = '35d9a306792f2de5075a4f32bfe09da6'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
